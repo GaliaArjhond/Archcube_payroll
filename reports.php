45e529a6649439abc2f71b1b53dd7f4ded95ed6d
GaliaArjhond <galiaarjhond0@gmail.com>
2025-05-09 14:13:33 +0800

    add the module and style

---
+++ reports.php
@@ -0,0 +1,47 @@
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="assets/css/reportsStyle.css" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Summary Reports</title>
+</head>
+
+<body>
+    <div class="side_bar">
+        <h1>Archcube Payroll</h1>
+        <div class="side_bar_container">
+            <div class="side_bar_item">
+                <a href="dashboard.html">Dashboard</a>
+            </div>
+            <div class="side_bar_item">
+                <a href="">Employee Management</a>
+            </div>
+            <div class="side_bar_item">
+                <a href="">Attendance</a>
+            </div>
+            <div class="side_bar_item">
+                <a href="">Payroll Management</a>
+            </div>
+            <div class="side_bar_item">
+                <a href="">Deductions & Benefits Management</a>
+            </div>
+            <div class="side_bar_item">
+                <a href="">Payslip Generator</a>
+            </div>
+            <div class="side_bar_item">
+                <a href="">Summary Reports</a>
+            </div>
+            <div class="side_bar_item">
+                <a href="">Settings</a>
+            </div>
+            <div class="side_bar_item">
+                <a href="" class="logout">Log Out</a>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
