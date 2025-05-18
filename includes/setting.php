3cf4756b51e084bd988fdde921ab44117151830b
GaliaArjhond <galiaarjhond0@gmail.com>
2025-05-18 11:06:13 +0800

    add setting front end

--- includes/setting.php
+++ includes/setting.php
@@ -44,8 +44,92 @@
     <div class="main_content">
         <div class="settings_form">
             <form action="setting_process.php" method="post">
-                <label for="">System Logs</label>
-                <a href="viewLogs.php">View Logs</a>
+                <div class="settings_card">
+                    <div class="settings_card_title">
+                        <span>⚙️</span> Account Settings
+                    </div>
+                    <div class="settings_row">
+                        <div class="settings_row_info">
+                            <span class="settings_row_title">Change Password</span>
+                            <span class="settings_row_desc">Update your login password</span>
+                        </div>
+                        <div class="settings_row_action">
+                            <a href="../includes/changePassword.php" class="settings_btn">Change</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="settings_card">
+                    <div class="settings_card_title">
+                        <span>🛠️</span> Admin Controls
+                    </div>
+                    <div class="settings_row">
+                        <div class="settings_row_info">
+                            <span class="settings_row_title">Manage Admins</span>
+                            <span class="settings_row_desc">Add or remove system administrators</span>
+                        </div>
+                        <div class="settings_row_action">
+                            <a href="../includes/adminControls.php" class="settings_btn">Manage</a>
+                        </div>
+                    </div>
+                    <div class="settings_row">
+                        <div class="settings_row_info">
+                            <span class="settings_row_title">System Logs</span>
+                            <span class="settings_row_desc">View activity history and login records</span>
+                        </div>
+                        <div class="settings_row_action">
+                            <a href="../includes/viewLogs.php" class="settings_btn">View Logs</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="settings_card">
+                    <div class="settings_card_title">
+                        <span>👷</span> Worker Settings
+                    </div>
+                    <div class="settings_row">
+                        <div class="settings_row_info">
+                            <span class="settings_row_title">Edit Positions</span>
+                            <span class="settings_row_desc">Update or add new job titles</span>
+                        </div>
+                        <div class="settings_row_action">
+                            <a href="../includes/manageWorkers.php" class="settings_btn">Edit</a>
+                        </div>
+                    </div>
+                    <div class="settings_row">
+                        <div class="settings_row_info">
+                            <span class="settings_row_title">Wage Management</span>
+                            <span class="settings_row_desc">Configure salaries, OT, and deductions</span>
+                        </div>
+                        <div class="settings_row_action">
+                            <a href="../includes/workerBenefits.php" class="settings_btn">Configure</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="settings_card">
+                    <div class="settings_card_title settings_delete_title">
+                        <span>🛡️</span> Delete
+                    </div>
+                    <div class="settings_row">
+                        <div class="settings_row_info">
+                            <span class="settings_row_title">Delete Worker</span>
+                            <span class="settings_row_desc">Permanently remove account and data</span>
+                        </div>
+                        <div class="settings_row_action">
+                            <a href="../includes/manageWorkers.php" class="settings_btn danger">Delete Account</a>
+                        </div>
+                    </div>
+                    <div class="settings_row">
+                        <div class="settings_row_info">
+                            <span class="settings_row_title">Delete This Account</span>
+                            <span class="settings_row_desc">Disable this account and data</span>
+                        </div>
+                        <div class="settings_row_action">
+                            <a href="../includes/workerBenefits.php" class="settings_btn danger">Delete this Account</a>
+                        </div>
+                    </div>
+                </div>
             </form>
         </div>
 
